d6d92e3572254e7d1a69ebaad9674cd97f71f848
Jonatan Sombié <jonatan.sombie@gmail.com>
2023-10-06 12:05:31 +0200

    rename stack name

--- bin/hereya-bootstrap.ts
+++ bin/hereya-bootstrap.ts
@@ -4,7 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import { HereyaBootstrapStack } from '../lib/hereya-bootstrap-stack';
 
 const app = new cdk.App();
-new HereyaBootstrapStack(app, 'HereyaBootstrapStack', {
+new HereyaBootstrapStack(app, 'HereyaBootstrap', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
