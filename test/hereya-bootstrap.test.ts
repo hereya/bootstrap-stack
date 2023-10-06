e2921982a30ad3f6f00823c4e78a34cbeededa79
Jonatan Sombié <jonatan.sombie@gmail.com>
2023-10-06 12:02:35 +0200

    rename stack

--- test/hereya-bootstrap.test.ts
+++ test/hereya-bootstrap.test.ts
@@ -1,13 +1,13 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as HereyaBootstrapStack from '../lib/hereya-bootstrap-stack-stack';
+// import * as HereyaBootstrapStack from '../lib/hereya-bootstrap-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/hereya-bootstrap-stack-stack.ts
+// example resource in lib/hereya-bootstrap-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
-//   const stack = new HereyaBootstrapStack.HereyaBootstrapStackStack(app, 'MyTestStack');
+//   const stack = new HereyaBootstrapStack.HereyaBootstrapStack(app, 'MyTestStack');
 //     // THEN
 //   const template = Template.fromStack(stack);
 
